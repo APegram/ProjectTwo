5addb31da850a8b9a928bf2061888f18a574bcc6
BlaiseAK <blaise.a.kress@gmail.com>
2018-07-18 20:13:44 -0400

    Added seeds.sql for new the userTable and comments tables

--- models/seeds copy.sql
+++ models/seeds copy.sql
@@ -1,10 +1,14 @@
-INSERT INTO user_Table (name, admin, password) VALUES ("Bob", false, "hello");
-INSERT INTO user_Table (name, admin, password) VALUES ("Jeff", true, "hello");
-INSERT INTO user_Table (name, admin, password) VALUES ("Pawel", false, "hello");
-INSERT INTO user_Table (name, admin, password) VALUES ("Doyle", true, "hello");
-INSERT INTO user_Table (name, admin, password) VALUES ("Matt", true, "hello");
-INSERT INTO user_Table (name, admin, password) VALUES ("Morgan", true, "hello");
-INSERT INTO user_Table (name, admin, password) VALUES ("Stewart", false, "hello");
-INSERT INTO user_Table (name, admin, password) VALUES ("Stephan", false, "hello");
-INSERT INTO user_Table (name, admin, password) VALUES ("Adam", false, "hello");
-INSERT INTO user_Table (name, admin, password) VALUES ("Blaise", false, "hello");
+INSERT INTO userTable (admin, name, userName, email, password) VALUES (false, "Bob", "mynameisbob", "bob@gmail.com", "hello");
+INSERT INTO userTable (admin, name, userName, email, password) VALUES (true, "Jeff", "mynameisjeff", "jeff@gmail.com", "hello");
+INSERT INTO userTable (admin, name, userName, email, password) VALUES (false, "Pawel", "mynameispawel", "pawel@gmail.com", "hello");
+INSERT INTO userTable (admin, name, userName, email, password) VALUES (true, "Doyle", "mynameisdoyle", "doyle@gmail.com", "hello");
+INSERT INTO userTable (admin, name, userName, email, password) VALUES (true, "Matt", "mynameismatt", "matt@gmail.com", "hello");
+INSERT INTO userTable (admin, name, userName, email, password) VALUES (true, "Morgan", "mynameismorgan", "morgan@gmail.com", "hello");
+INSERT INTO userTable (admin, name, userName, email, password) VALUES (false, "Stewart", "mynameisstewart", "stewar@gmail.com", "hello");
+INSERT INTO userTable (admin, name, userName, email, password) VALUES (false, "Stephen", "mynameisstephen", "stephen@gmail.com", "hello");
+INSERT INTO userTable (admin, name, userName, email, password) VALUES (false, "Adam", "mynameisadam", "adam@gmail.com", "hello");
+INSERT INTO userTable (admin, name, userName, email, password) VALUES (false, "Blaise", "mynameisblaise", "blaise@gmail.com", "hello");
+
+
+INSERT INTO comments (text, name, stepNum) VALUES ("Help me TAs! I'm no where close to Blaise!", "Stephen", 1);
+INSERT INTO comments (text, name, stepNum) VALUES ("Help me TAs! I wish I was as good as Blaise!", "Stewart", 0);
